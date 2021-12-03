5b9f8eb48cc0527bd6d47fe449b0ac2c885ed1b9
Giulia <giu.olivers@gmail.com>
2021-12-03 11:59:21 -0300

    Configures App.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,10 @@ import ProductController from "../controllers/ProductController";
 
 const routes = new Router();
 
+routes.get("/products", ProductController.index);
+routes.get("/products/:id", ProductController.read);
+routes.post("/products", ProductController.create);
+routes.put("/products/:id", ProductController.update);
+routes.delete("/products/:id", ProductController.delete);
+
 export default routes;
